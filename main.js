dedca5542d62e0dc2d567223807379da8dff765c
Benjamin Huang <coolconfucius@yahoo.com>
2016-01-22 21:51:47 -0800

    cleaned code

--- main.js
+++ main.js
@@ -3,15 +3,6 @@
 var main = {
   init: function(){
     $('#convert').click(main.render);
-    $('#clean').click(main.clean);
-    $('#rendered').on('.converted', 'click', main.remove);
-
-    // $('#exSquare').click(main.exSquare);
-    // $('#exAdd').click(main.exAdd);
-    // $('#exSentence').click(main.exSentence);
-    // $('#exGravatar').click(main.exGravatar);
-
-    $('.example').click(getExample); 
   }, 
 
   render: function(){
@@ -32,29 +23,7 @@ var main = {
     .fail(function(err) {
       alert('something went wrong :(')
     });
-  },
-
-  clean: function(){
-   console.log("clean");
-    $('#rendered').empty();  
-  },
-
-  remove: function(){
-    $(this).remove(); 
-  }, 
-
-  examples: [
-    'I am using __markdown__.', 
-    '# Marked in browser\n\nRendered by **marked**.',
-    '# h1 ## h2 ### h3 #### h4'
-  ], 
-
-  getExample: function(){
-    console.log(main.examples);
-    console.log('getExample');
-    $('#markdown').attr('value', main.examples[parseInt($(this).text())]);
   }
-
 }
 
 $(document).ready(init);
